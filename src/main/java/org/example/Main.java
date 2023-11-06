7b84aa5668d65f27f442fe0b54c33abaf55b49fc
Sug4chy <sb.9226981433@gmail.com>
2023-11-06 09:35:12 +0500

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws InterruptedException {
+        // создаем пул для выполнения наших задач
+        //   максимальное количество созданных задач - 3
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                // не изменяйте эти параметры
+                3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(3));
+
+        // сколько задач выполнилось
+        AtomicInteger count = new AtomicInteger(0);
+
+        // сколько задач выполняется
+        AtomicInteger inProgress = new AtomicInteger(0);
+
+        // отправляем задачи на выполнение
+        for (int i = 0; i < 30; i++) {
+            final int number = i;
+            System.out.println("creating #" + i);
+            while (executor.getQueue().size() == 3) {
+                Thread.sleep(10);
+            }
+            executor.submit(() -> {
+                int working = inProgress.incrementAndGet();
+                System.out.println("start #" + number + ", in progress: " + working);
+                try {
+                    // тут какая-то полезная работа
+                    Thread.sleep(Math.round(1000 + Math.random() * 2000));
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+                working = inProgress.decrementAndGet();
+                System.out.println("end #" + number + ", in progress: " + working + ", done tasks: " + count.incrementAndGet());
+            });
+        }
+        executor.close();
     }
 }
